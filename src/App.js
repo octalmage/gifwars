4c9ba78e67d570bb3efe379ccc3454943f4ca809
Hamilton Shu <hamilton.shu@wpengine.com>
2018-02-05 10:21:18 -0600

    react router and title page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Grid, Row, Col} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={12} md={12}><h1 class="center">GifWars</h1></Col>
+        </Row>
+      </Grid>
     );
   }
 }
