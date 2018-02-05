9ab4e8313c1f5006e67c01152c377d93d8abd96d
Jason Stallings <jason@stallin.gs>
2018-02-05 11:36:21 -0600

    Firebase function to start a game.
    Co-Authored-By: Kopczynski <kopczynski@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.games = functions.https.onRequest((request, response) => {
+  switch (req.method) {
+    case 'POST':
+      startGame(request.query.player);
+      break;
+    default:
+      res.status(418).send({ error: "I'M A TEAPOT" });
+      break;
+  }
+});
+
+function startGame(owner) {
+  const roomcode = makeid(4);
+  admin.database().ref(`/games/${roomcode}`).set({ players: [owner] }).then(() => {
+    response.send({ roomcode: roomcode });
+  });
+}
+
+function makeid(len) {
+  const text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < len; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
