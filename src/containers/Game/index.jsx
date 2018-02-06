b0e2cff1e53e6bfb15c3c15387abf6d73bae4bb7
Hamilton Shu <hamilton.shu@wpengine.com>
2018-02-06 10:45:57 -0600

    move timer into the component instead of the parent, keep all logic in the component

--- src/containers/Game/index.jsx
+++ src/containers/Game/index.jsx
@@ -18,46 +18,14 @@ class Game extends React.Component {
       round: 1,
       prompt: 'trains'
     }
-    this.state.countdown = 0;
-    this.setupTimer();
   }
 
-  updateTime() {
-    //cloud function for time
-    return (new Date()).getTime();
-  }
-
-  setupTimer() {
-    this.setState(
-      {currentTime: this.updateTime(),
-      countdown: Math.round((this.state.game.expire - this.updateTime())/1000)}
-    );
-    this.timerInterval = setInterval(
-      () => {
-        if (this.state.game.expire < this.state.currentTime) {
-          this.setState(
-            {currentTime: this.updateTime(),
-            countdown: 0}
-          );
-          clearInterval(this.timerInterval);
-          return;
-        }
-        this.setState(
-          {currentTime: this.updateTime(),
-          countdown: Math.round((this.state.game.expire - this.updateTime())/1000)}
-        );
-      }, 500
-    );
-  }
 
   render() {
     return (
       <Grid>
         <Row className="center">
           <h1> {this.state.user.name + ' in ' + this.state.game.id + ' on Round ' + this.state.game.round } </h1>
-        </Row>
-        <Row className="center">
-          <h2>{ this.state.countdown } seconds remaining</h2>
         </Row>
         <Row>
           <GifSearch user={this.state.user} game={this.state.game}/>
